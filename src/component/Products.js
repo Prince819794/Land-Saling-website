7369e24912adf5a04115e1d7d4d3c91c7a39278d
samrat <samratborah111@gmail.com>
2021-11-27 19:53:35 +0530

    done with frontend

--- src/component/Products.js
+++ src/component/Products.js
@@ -8,14 +8,16 @@ const Products = () => {
     <Wrapper>
       <Heading>Find your next place to live</Heading>
       <Wrap>
-      <Selects>
-        <Select placeholder="Looking For"/>
-        <Select placeholder="Property type"/>
-        <Select placeholder="Type"/>
-      </Selects>
-      <SearchBox>
-        <Search />
-      </SearchBox>
+        <Selects>
+          <Select placeholder="Looking For" />
+          <Select placeholder="Property type" />
+          <Select placeholder="Type" />
+        </Selects>
+
+        <SearchBox>
+          <Search />
+        </SearchBox>
+        <Button>search</Button>
       </Wrap>
     </Wrapper>
   );
@@ -30,9 +32,11 @@ const Wrapper = styled.div`
   border-radius: 5px;
 `;
 const Heading = styled.h3`
+  font-size: 2rem;
   font-weight: 500;
   letter-spacing: 0.6px;
   margin-bottom: 20px;
+  margin-left: 4rem;
 `;
 
 const Selects = styled.div`
@@ -42,9 +46,19 @@ const Selects = styled.div`
 `;
 const SearchBox = styled.div``;
 const Wrap = styled.div`
-display: flex;
-justify-content: space-around;
-padding: 9px 20px;
-background-color: #f5f5f5;
-border-radius: 5px;
+  display: flex;
+  justify-content: space-around;
+  padding: 20px 5px;
+  margin: 1.3rem;
+  background-color: #e4e4ee;
+  border-radius: 5px;
+`;
+const Button = styled.button`
+  width: 10%;
+  border-radius: 10px;
+  border-style: none;
+  cursor: pointer;
+  background-color: #833ab4;
+  color: #fff;
+  font-size: 1.2rem;
 `;
